530a0a6012743826bc4c49eeb7204b594305ac7f
carlos carrasco <carlos.carrasco@strata-analytics.us>
2022-10-27 15:01:31 -0300

    Initial commit

---
+++ src/AttributePanel/components/blocks/Page/index.tsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import {
+  ColorPickerField,
+  InputWithUnitField,
+  SwitchField,
+  TextAreaField,
+  TextField,
+} from '@extensions/components/Form';
+import { Help } from '@extensions/AttributePanel/components/UI/Help';
+import { AddFont } from '@extensions/components/Form/AddFont';
+import { Collapse, Grid, Space } from '@arco-design/web-react';
+import { Stack, TextStyle, useFocusIdx } from 'easy-email-editor';
+import { AttributesPanelWrapper } from '@extensions/AttributePanel/components/attributes/AttributesPanelWrapper';
+import { FontFamily } from '../../attributes/FontFamily';
+
+export function Page() {
+  const { focusIdx } = useFocusIdx();
+
+  if (!focusIdx) return null;
+  return (
+    <AttributesPanelWrapper style={{ padding: 0 }}>
+      <Stack.Item fill>
+        <Collapse defaultActiveKey={['0', '1']}>
+          <Collapse.Item
+            name='0'
+            header={t('Email Setting')}
+          >
+            <Space direction='vertical'>
+              <TextField
+                label={t('Subject')}
+                name={'subject'}
+                inline
+              />
+              <TextField
+                label={t('SubTitle')}
+                name={'subTitle'}
+                inline
+              />
+              <InputWithUnitField
+                label={t('Width')}
+                name={`${focusIdx}.attributes.width`}
+                inline
+              />
+              <InputWithUnitField
+                label={t('Breakpoint')}
+                helpText={t(
+                  'Allows you to control on which breakpoint the layout should go desktop/mobile.',
+                )}
+                name={`${focusIdx}.data.value.breakpoint`}
+                inline
+              />
+            </Space>
+          </Collapse.Item>
+          <Collapse.Item
+            name='1'
+            header={t('Theme Setting')}
+          >
+            <Stack
+              vertical
+              spacing='tight'
+            >
+              <Grid.Row>
+                <Grid.Col span={11}>
+                  <FontFamily name={`${focusIdx}.data.value.font-family`} />
+                </Grid.Col>
+                <Grid.Col
+                  offset={1}
+                  span={11}
+                >
+                  <InputWithUnitField
+                    label={t('Font size')}
+                    name={`${focusIdx}.data.value.font-size`}
+                  />
+                </Grid.Col>
+              </Grid.Row>
+
+              <Grid.Row>
+                <Grid.Col span={11}>
+                  <InputWithUnitField
+                    label='Line height'
+                    unitOptions='percent'
+                    name={`${focusIdx}.data.value.line-height`}
+                  />
+                </Grid.Col>
+                <Grid.Col
+                  offset={1}
+                  span={11}
+                >
+                  <InputWithUnitField
+                    label={t('Font weight')}
+                    unitOptions='percent'
+                    name={`${focusIdx}.data.value.font-weight`}
+                  />
+                </Grid.Col>
+              </Grid.Row>
+
+              <Grid.Row>
+                <Grid.Col span={11}>
+                  <ColorPickerField
+                    label={t('Text color')}
+                    name={`${focusIdx}.data.value.text-color`}
+                  />
+                </Grid.Col>
+                <Grid.Col
+                  offset={1}
+                  span={11}
+                >
+                  <ColorPickerField
+                    label={t('Background')}
+                    name={`${focusIdx}.attributes.background-color`}
+                  />
+                </Grid.Col>
+              </Grid.Row>
+
+              <Grid.Row>
+                <ColorPickerField
+                  label={t('Content background')}
+                  name={`${focusIdx}.data.value.content-background-color`}
+                />
+              </Grid.Row>
+
+              <TextAreaField
+                autoSize
+                label={t('User style')}
+                name={`${focusIdx}.data.value.user-style.content`}
+              />
+              <Stack.Item />
+              <Stack.Item />
+              <AddFont />
+              <Stack.Item />
+              <Stack.Item />
+            </Stack>
+          </Collapse.Item>
+        </Collapse>
+      </Stack.Item>
+    </AttributesPanelWrapper>
+  );
+}
---
+++ src/AttributePanel/utils/BlockAttributeConfigurationManager.ts
@@ -0,0 +1,21 @@
+import { blocks } from '../components/blocks';
+
+type ObjectComponent = { [key: string]: () => JSX.Element | null };
+
+export class BlockAttributeConfigurationManager {
+  private static map: ObjectComponent = { ...blocks };
+
+  public static add(componentMap: ObjectComponent) {
+    Object.keys(componentMap).forEach(name => {
+      this.map[name] = componentMap[name];
+    });
+  }
+
+  public static get<T extends ObjectComponent>(name: keyof T): () => JSX.Element | null {
+    return (this.map as any)[name];
+  }
+
+  public static getMap() {
+    return this.map;
+  }
+}
